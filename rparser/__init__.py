7050dfd6d7794cf379e996eb9677febd9b332fe8
johnmwood <jmw.home@gmail.com>
2018-10-15 10:21:39 -0600

    Updated setup.py with correct arguments

--- rparser/__init__.py
+++ rparser/__init__.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 setup( 
     # Needed to silence warnings (and to be a worthwhile package)
-    name='Measurements',
-    url='https://github.com/jladan/package_demo',
+    name='RParser',
+    url='https://github.com/HSPS-DataScience/RParser'
     author='John Ladan',
-    author_email='jladan@uwaterloo.ca',
+    author_email='jmw.home@gmail.com',
     # Needed to actually package something
-    packages=['measure'],
+    packages=['rparser'],
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=['itertools', 're'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
-    license='MIT',
-    description='An example of a python package from pre-existing code',
+    license='',
+    description='Parse an R script into Rmd using Python',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
