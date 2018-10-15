6979cec80ed831ab679731cffc1a53757b5da842
johnmwood <jmw.home@gmail.com>
2018-10-15 10:22:09 -0600

    Added setup to setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup 
+
+setup( 
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='RParser',
+    url='https://github.com/HSPS-DataScience/RParser'
+    author='John Ladan',
+    author_email='jmw.home@gmail.com',
+    # Needed to actually package something
+    packages=['rparser'],
+    # Needed for dependencies
+    install_requires=['itertools', 're'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='',
+    description='Parse an R script into Rmd using Python',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
